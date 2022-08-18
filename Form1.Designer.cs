3c3e83ed9838b8aeeb0dff96a91c683e0d799fb4
Liron <110838700+ladunjexa@users.noreply.github.com>
2022-08-18 05:31:54 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Encrypter
+{
+    partial class YL16Encryption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.softwareTitle = new System.Windows.Forms.Label();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Credits = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Divider = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 46);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(241, 20);
+            this.Input.TabIndex = 0;
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(259, 44);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(103, 23);
+            this.Encrypt.TabIndex = 2;
+            this.Encrypt.Text = "Encrypt";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // softwareTitle
+            // 
+            this.softwareTitle.AutoSize = true;
+            this.softwareTitle.Font = new System.Drawing.Font("Tahoma", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.softwareTitle.ForeColor = System.Drawing.Color.DarkBlue;
+            this.softwareTitle.Location = new System.Drawing.Point(123, 8);
+            this.softwareTitle.Name = "softwareTitle";
+            this.softwareTitle.Size = new System.Drawing.Size(209, 29);
+            this.softwareTitle.TabIndex = 4;
+            this.softwareTitle.Text = "YL16 Encryption";
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(364, 44);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(103, 23);
+            this.Decrypt.TabIndex = 6;
+            this.Decrypt.Text = "Decrypt";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 118);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(448, 90);
+            this.Output.TabIndex = 7;
+            // 
+            // Credits
+            // 
+            this.Credits.AutoSize = true;
+            this.Credits.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Credits.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Credits.Location = new System.Drawing.Point(17, 75);
+            this.Credits.Name = "Credits";
+            this.Credits.Size = new System.Drawing.Size(161, 26);
+            this.Credits.TabIndex = 8;
+            this.Credits.Text = "(c) Developed by [T]he3DeVi[L] \r\n(younglirz16@gmail.com)";
+            this.Credits.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Clear.ForeColor = System.Drawing.Color.Gray;
+            this.Clear.Location = new System.Drawing.Point(233, 214);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(103, 23);
+            this.Clear.TabIndex = 9;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Copy.ForeColor = System.Drawing.Color.Gray;
+            this.Copy.Location = new System.Drawing.Point(125, 214);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(103, 23);
+            this.Copy.TabIndex = 10;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(259, 70);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(103, 23);
+            this.Paste.TabIndex = 12;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Divider
+            // 
+            this.Divider.AutoSize = true;
+            this.Divider.Location = new System.Drawing.Point(15, 101);
+            this.Divider.Name = "Divider";
+            this.Divider.Size = new System.Drawing.Size(439, 13);
+            this.Divider.TabIndex = 13;
+            this.Divider.Text = "________________________________________________________________________\r\n";
+            // 
+            // YL16Encryption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 245);
+            this.Controls.Add(this.Divider);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Credits);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.softwareTitle);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.Input);
+            this.Name = "YL16Encryption";
+            this.Text = "YL16 Encryption (Developed by T3D)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.Label softwareTitle;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Label Credits;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Label Divider;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿//==============================================================================
+/*
+ *            Liron Encryption Application 1.0
+ *       (c) Copyright 2012-2013 by Liron
+ *
+ * @author    : Liron
+ * @date      : 26th October 2013
+ * @update    : none
+ * @team      : none
+ *
+ * This file is provided as is (no warranties).
+ */
+//============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Encrypter
+{
+    public partial class YL16Encryption : Form
+    {
+        DataEncryptor keys = new DataEncryptor();
+        public YL16Encryption()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Input.Text)) 
+                MessageBox.Show("Enter text to encrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else Output.Text = keys.EncryptString(Input.Text);
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Input.Text)) 
+                MessageBox.Show("Enter code to decrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else Output.Text = keys.DecryptString(Input.Text);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls) if (c is TextBox) ((TextBox)c).Clear();
+        }
+        private void button4_Click(object sender, EventArgs e) { Clipboard.SetText(Output.Text); }
+        private void button6_Click(object sender, EventArgs e) { Input.Text = Clipboard.GetText(); }
+        private void label3_Click(object sender, EventArgs e) { }
+
+        private Random _random = new Random(Environment.TickCount);
+
+        public string RandomString(int length)
+        {
+            string chars = @"0123456789abcdefghijklmnopqrstuvwxyz!@#$%^|&*()_+=-,.:';/\";
+            StringBuilder builder = new StringBuilder(length);
+
+            for (int i = 0; i < length; ++i)
+                builder.Append(chars[_random.Next(chars.Length)]);
+
+            return builder.ToString();
+        }
+
+    }
+
+    public class DataEncryptor
+    {
+        TripleDESCryptoServiceProvider symm;
+
+        #region Factory
+        public DataEncryptor()
+        {
+            this.symm = new TripleDESCryptoServiceProvider();
+            this.symm.Padding = PaddingMode.PKCS7;
+        }
+        public DataEncryptor(TripleDESCryptoServiceProvider keys)
+        { this.symm = keys; }
+
+        public DataEncryptor(byte[] key, byte[] iv)
+        {
+            this.symm = new TripleDESCryptoServiceProvider();
+            this.symm.Padding = PaddingMode.PKCS7;
+            this.symm.Key = key;
+            this.symm.IV = iv;
+        }
+
+        #endregion
+
+        #region Properties
+        public TripleDESCryptoServiceProvider Algorithm
+            { get { return symm; } set { symm = value; } }
+        public byte[] Key
+            { get { return symm.Key; } set { symm.Key = value; } }
+        public byte[] IV
+            { get { return symm.IV; } set { symm.IV = value; } }
+
+        #endregion
+
+        #region Crypto
+
+        public byte[] Encrypt(byte[] data) { return Encrypt(data, data.Length); }
+        public byte[] Encrypt(byte[] data, int length)
+        {
+            try
+            {
+                // Create a MemoryStream.
+                var ms = new MemoryStream();
+
+                // Create a CryptoStream using the MemoryStream 
+                // and the passed key and initialization vector (IV).
+                var cs = new CryptoStream(ms,
+                    symm.CreateEncryptor(symm.Key, symm.IV),
+                    CryptoStreamMode.Write);
+
+                // Write the byte array to the crypto stream and flush it.
+                cs.Write(data, 0, length);
+                cs.FlushFinalBlock();
+
+                // Get an array of bytes from the 
+                // MemoryStream that holds the 
+                // encrypted data.
+                byte[] ret = ms.ToArray();
+
+                // Close the streams.
+                cs.Close();
+                ms.Close();
+
+                // Return the encrypted buffer.
+                return ret;
+            }
+            catch (CryptographicException ex)
+                { Console.WriteLine("A cryptographic error occured: {0}", ex.Message); }
+            return null;
+        }
+
+        public string EncryptString(string text)
+            { return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(text))); }
+
+        public byte[] Decrypt(byte[] data) { return Decrypt(data, data.Length); }
+        public byte[] Decrypt(byte[] data, int length)
+        {
+            try
+            {
+                // Create a new MemoryStream using the passed 
+                // array of encrypted data.
+                MemoryStream ms = new MemoryStream(data);
+
+                // Create a CryptoStream using the MemoryStream 
+                // and the passed key and initialization vector (IV).
+                CryptoStream cs = new CryptoStream(ms,
+                    symm.CreateDecryptor(symm.Key, symm.IV),
+                    CryptoStreamMode.Read);
+
+                // Create buffer to hold the decrypted data.
+                byte[] result = new byte[length];
+
+                // Read the decrypted data out of the crypto stream
+                // and place it into the temporary buffer.
+                cs.Read(result, 0, result.Length);
+                return result;
+            }
+            catch (CryptographicException ex)
+                { Console.WriteLine("A cryptographic error occured: {0}", ex.Message); }
+            return null;
+        }
+
+        public string DecryptString(string data)
+            { return Encoding.UTF8.GetString(Decrypt(Convert.FromBase64String(data))).TrimEnd('\0'); }
+
+        #endregion
+
+    }
+}
